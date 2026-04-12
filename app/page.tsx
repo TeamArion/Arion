547273816079d1ce7f27a973413dfefd75fb1224
ardev-c <aksnmit@gmail.com>
2026-04-12 21:34:42 +0530

    Fixed 404 Error message and added a landing page

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Hero from "./components/landing/Hero";
+import About from "./components/landing/About";
+import MissionVision from "./components/landing/MissionVision";
+import AboutInstitution from "./components/landing/AboutInstitution";
+
+
+export default function Home() {
+  return (
+    <div className="pt-0 bg-black">
+      <Hero />
+      <About />
+      <AboutInstitution />
+      <MissionVision />
+    </div>
+  );
+}
